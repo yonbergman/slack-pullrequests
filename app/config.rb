781feecea2ee157df26e5528cdf5f068af6ffc22
yon <Lighthawky@gmail.com>
2015-01-05 17:06:10 +0200

    forgot ,

--- app/config.rb
+++ app/config.rb
@@ -2,9 +2,7 @@ require 'ostruct'
 
 $config = OpenStruct.new(
   github_token: ENV['GITHUB_TOKEN'],
-
-  repos: ENV['GITHUB_REPOS'].split(/\s/)
-
+  repos: ENV['GITHUB_REPOS'].split(/\s/),
   users: {
     'yonbergman' => 'yonbergman',
     'sdavidson' => 'ShayDavidson',
