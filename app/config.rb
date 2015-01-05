e4bbcf08d81efb99d2c16da261e7ddbc65b09b4f
yon <Lighthawky@gmail.com>
2015-01-05 17:04:44 +0200

    better config

---
+++ app/config.rb
@@ -0,0 +1,15 @@
+require 'ostruct'
+
+$config = OpenStruct.new(
+  github_token: ENV['GITHUB_TOKEN'],
+
+  repos: ENV['GITHUB_REPOS'].split(/\s/)
+
+  users: {
+    'yonbergman' => 'yonbergman',
+    'sdavidson' => 'ShayDavidson',
+    'benny' => 'gardenofwine',
+    'assafgelber' => 'agelber',
+    'udi' => 'udiWertheimer',
+  }
+)
